5668334d75adb742efa64da168b9e14776b7f103
rajthilak <rajthilak04@gmail.com>
2023-05-16 17:42:10 +0530

    COmponents created statically

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeListsComponent } from './recipes/recipe-lists/recipe-lists.component';
+import { RecipeDetailsComponent } from './recipes/recipe-details/recipe-details.component';
+import { RecipeItemComponent } from './recipes/recipe-lists/recipe-item/recipe-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    RecipesComponent,
+    RecipeListsComponent,
+    RecipeDetailsComponent,
+    RecipeItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/recipes/recipe-lists/recipe-lists.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-lists',
+  templateUrl: './recipe-lists.component.html',
+  styleUrls: ['./recipe-lists.component.css']
+})
+export class RecipeListsComponent {
+
+}
